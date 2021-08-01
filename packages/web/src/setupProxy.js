a7a73873858863f5ce7ffc9ea02c8444aa5f1bd8
Maksym Tkachuk <uakorn@gmail.com>
2021-08-01 17:11:00 +0300

    Setup proxy and add CRA template

---
+++ packages/web/src/setupProxy.js
@@ -0,0 +1,14 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+    app.use(
+        '/api',
+        createProxyMiddleware({
+            target: 'http://api:8080',
+            changeOrigin: true,
+            pathRewrite: {
+                '^/api': '/'
+            }
+        })
+    );
+};
